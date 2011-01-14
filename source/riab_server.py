d0233e49a2af066fce3bdb7ce2f8871a3a531abb
Ted Dunstone <teddunstone@ubuntu.(none)>
2011-01-14 17:52:16 +0700

    inital commit

---
+++ source/riab_server.py
@@ -0,0 +1,8 @@
+## RIAB_SERVER CLASS
+
+## Risk-in-a-Box (RIAB) Server is a Python package for modelling  
+## the impact from natural hazards.
+
+## The server is a stateless server that exposes an XMLRPC API
+## and interfaces using REST with any GeoServers
+
