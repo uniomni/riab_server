5a5675af2781c99059a7cfcf422df208dd327d15
uniomni Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2011-01-17 12:09:38 +0700

    Added test that fails

--- tests/test_geoserver_api.py
+++ tests/test_geoserver_api.py
@@ -24,6 +24,8 @@ class Test_API(unittest.TestCase):
         pass
 
     def test_geoserver_layer_handles(self):
+        """Test that layer handles are correctly formed and unpacked"""
+        
         username='alice'
         userpass='cooper'
         geoserver_url='schools.out.forever'
@@ -36,6 +38,21 @@ class Test_API(unittest.TestCase):
         assert s2 == userpass
         assert s3 == geoserver_url        
         assert s4 == layer_name
+
+    def test_connection_to_geoserver(self):
+        """Test that geoserver can be reached using layer handle"""
+        
+        # FIXME(Ole): I think these should be defaults e.g. in config.py
+        geoserver_url = 'http://localhost:8080/geoserver'
+        username = 'admin'
+        userpass = 'geoserver'
+        layer_name = 'tasmania_roads'
+
+        lh = self.api.create_geoserver_layer_handle(username, userpass, geoserver_url, layer_name)        
+        res = self.api.check_geoserver_layer_handle(lh) 
+        
+        msg = 'Was not able to access Geoserver layer: %s' % lh
+        assert res == 'SUCCESS', msg               
         
         
 ################################################################################
